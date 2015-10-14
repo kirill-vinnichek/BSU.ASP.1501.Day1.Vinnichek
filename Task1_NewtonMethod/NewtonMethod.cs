348c2948bab3a0973b28b99cd5401a81350a6154
kirill-vinnichek <kirill.vinnichek@gmail.com>
2015-10-14 20:19:26 +0300

    Some refactoring

--- Task1_NewtonMethod/NewtonMethod.cs
+++ Task1_NewtonMethod/NewtonMethod.cs
@@ -11,16 +11,17 @@ namespace Task1_NewtonMethod
 
         public static double RootExtract(double A, int n, double e)
         {
-            if (A == Double.NaN && n == Double.NaN && e == Double.NaN)
+            if (Double.IsNaN(A) && Double.IsNaN(n) && Double.IsNaN(e))
+                return Double.NaN;
+            if (A < 0 && n % 2 == 0)
                 return Double.NaN;
             if (n == 0)
                 return 1;
 
-            double x = A, x0 = A;
+            double x = A, x0;
 
             do
             {
-
                 x0 = x;
                 x = 1.0 / n * ((n - 1) * x0 + (A / Math.Pow(x0, n - 1)));
 
--- UIForTests/Program.cs
+++ UIForTests/Program.cs
@@ -11,13 +11,13 @@ namespace UIForTests
     {
         static void Main(string[] args)
         {
-            //double x = 9;
-            //int n = 2;
-            //double e = 0.0001;
+            double x = 27;
+            int n = 3;
+            double e = 0.0001;
 
-            //double a = NewtonMethod.RootExtract(x, n, e);
-            //double b = Math.Pow(x, 1.0 / n);
-            //Console.WriteLine("NewtonMethod - {0}\n Math.Pow - {1}", a, b);
+            double a = NewtonMethod.RootExtract(x, n, e);
+            double b = Math.Pow(x, 1.0 / n);
+            Console.WriteLine("NewtonMethod - {0}\n Math.Pow - {1}", a, b);
             var ar = new int[5][];
              ar[2]= new int[]{2,3};
              ar[3] = new int[]{-60,-1,9};
